4e0bbe40464311010a8022b92799094bf048c258
Lucas Peres <lucaspg96@live.com>
2018-12-05 11:54:32 -0300

    Removing old interface code

--- src/main/webapp/spa/page-title/page-title.js
+++
@@ -1,7 +0,0 @@
-function spaPageTitle(caption, title) {
-	return ''
-		+ '<div class="page-title">'
-		+     '<label id="page-label">' + caption + '</label>'
-		+     '<h1 id="page-title">' + title + '</h1>'
-		+ '</div>'
-};
